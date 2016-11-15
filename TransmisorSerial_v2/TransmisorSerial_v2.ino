f39cd91aa46c017eae2d02c684d43f14469b543e
ElectroPro <jopir70@gmail.com>
2016-11-15 10:47:18 -0500

    Update TransmisorSerial_v2.ino

--- TransmisorSerial_v2/TransmisorSerial_v2.ino
+++ TransmisorSerial_v2/TransmisorSerial_v2.ino
@@ -2,8 +2,8 @@
  * La trama debe tener el formato: -12.5, 15.6, -0.7, 26.0,  3.6, 27.1$
  * Se sugiere que la cantidad de caracteres entre cada coma "," sea el 
  * mismo para mantener un orden(esto puede variar sin generar error en el progama).
- * En este ejemplo cada número consta de cinco caracteres, por lo cual
- * se pueden enviar números en el rango de -99.9 a 999.9
+ * En este ejemplo cada número consta de siete caracteres, por lo cual
+ * se pueden enviar números en el rango de -999.99 a 9999.99
  * La trama enviada requiere de un dolar "$" para indicar el final de la trama
  */
 
