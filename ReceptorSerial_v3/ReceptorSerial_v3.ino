66d2de0af97f7ea405bbdc7a6e2dd454989d78c5
ElectroPro <jopir70@gmail.com>
2016-11-15 13:08:53 -0500

    Soporta Tramas de hasta 240 Caracteres

--- ReceptorSerial_v3/ReceptorSerial_v3.ino
+++ ReceptorSerial_v3/ReceptorSerial_v3.ino
@@ -1,25 +1,23 @@
 /* Recepción de Trama Serial haciendo uso de interrupción por puerto serial.
  * Primero captura la trama serial en una variable tipo String y luego 
  * la analiza con la función AnalizarTramaSerial()
- * Se espera un mensaje en el formato: -12.5, 15.6, -0.7, 26.0,  3.6, 27.1$
+ * Se espera un mensaje en el formato:  12.50, -15.60,  26.00, 132.50,   3.60,  27.10$
  * Este programa requiere un $(dolar) para indicar el final de la trama
  * Interrupción por puerto Serial no es compatible con Arduino Leonardo, Esplora o Micro. 
+ * Trama Serial de Prueba (240 caracteres --> 30 número tipo float):  
+ *  87.89, -89.67, -57.54,  17.96, -28.13,  48.01,  75.33,  62.01,  -2.75, -18.97, -39.11,  16.52,  47.23,  65.26, -41.15,  -2.59, -82.25,  50.88, -97.30,  44.44,  70.12,  39.40,  47.11,  74.62, -97.52,  28.90, -92.81, -33.87, -22.03, -62.69$
  */
 
-const int NUMERO_DE_DATOS = 6;    // numero de datos esperados
-int fieldIndex = 0;               // el actual campo que está siendo recibido
-String values[NUMERO_DE_DATOS];   // array que contiene los valores de todos los datos en formato String
-
-float Datos_Recibidos[NUMERO_DE_DATOS];
+String Trama = ""; // almacenamos los caracteres recibidos en la variable Trama para liberar el buffer de recepción.
+const int NUMERO_DE_DATOS = 30;    // numero de datos esperados (no considerar más de 30 datos, ya que la memoria dinámica del Arduino UNO es sólo de 2KB) 
+float Datos_Recibidos[NUMERO_DE_DATOS];  //array de número tipo float
 boolean datos_disponibles = false;
-
 boolean final_de_trama = false;
-String Trama = "";
+                
 
 void setup()
 {
   Serial.begin(9600);       // inicializa el puerto serial a 9600 baudios
-  pinMode(13,OUTPUT);       // Seteamos el PIN 13 como salida
 }
 
 void loop()
@@ -28,10 +26,7 @@ void loop()
     //Serial.println(Trama);
     AnalizarTramaSerial(Trama);
     if(datos_disponibles == true){
-      digitalWrite(13,HIGH);  // encendemos el led del pin 13 para indicar que hemos recibido datos
-      delay(250);
-      digitalWrite(13,LOW);  // apagamos el led
-      datos_disponibles == false; 
+      // Procesamos los datos almacenados en Datos_Recibidos[]
     }
     Trama = "";
     final_de_trama = false;
@@ -52,11 +47,11 @@ void serialEvent() {  // Interrupción del puerto Serial. Función que es llamad
 
 void AnalizarTramaSerial(String trama_serial){
 /* Función que captura los datos de tipo float dentro de una trama separada por comas
- * con un formato del tipo: -12.5, 15.6, -0.7, 26.0,  3.6, 27.1$
+ * con un formato del tipo:  12.50, -15.60,  26.00, 132.50,   3.60,  27.10$
  */
    const int TAMANO_DE_TRAMA = trama_serial.length();
    int fieldIndex = 0; // índice inicial de los campos recibidos.
-   char Datos[NUMERO_DE_DATOS]; // array que contendrá los valores de cada uno de los campos
+   String Datos[NUMERO_DE_DATOS]; // array que contendrá los valores de cada uno de los campos
    char ch;
    
    char Trama[TAMANO_DE_TRAMA];
@@ -80,7 +75,8 @@ void AnalizarTramaSerial(String trama_serial){
             //////////////////////////////////////////////////////////////////////////////////
             //////////  Convierte los caracteres recibidos en variables tipo float ///////////
             //////////////////////////////////////////////////////////////////////////////////
-            Datos_Recibidos[i] = values[i].toFloat();
+            Datos_Recibidos[i] = Datos[i].toFloat();
+            Serial.println(Datos_Recibidos[i]);  // <---- comentar luego de realizar las pruebas
             //////////////////////////////////////////////////////////////////////////////////
             datos_disponibles = true;
             }
